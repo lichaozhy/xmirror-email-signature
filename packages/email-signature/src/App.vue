d5a77163ece75f547dae8e50062947bd3eb298ed
chaoslee <chaos2lee@qq.com>
2022-12-02 11:26:49 +0800

    @xmirror/email-signature: [FIXED] addrerss field [FIXED] for mobile

--- packages/email-signature/src/App.vue
+++ packages/email-signature/src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import { Legacy } from './Solution/index.mjs';
 import { base64ToBlob } from './utils.mjs';
+import addressList from './address.json';
 
 const data = ref({
 	name: '姓名',
@@ -34,71 +35,91 @@ async function download() {
 
 <template>
 <b-container
-	fluid
 	class="h-100 p-0 d-flex align-items-center justify-content-center"
 >
-	<component
-		class="d-flex"
-		:is="solutionList[0].NameCard"
-		:name="data.name"
-		:position="data.position"
-		:telephone="data.telephone"
-		:email="data.emailName"
-		:homeURL="data.homeURL"
-		:address="data.address"
-		ref="solution"
-	/>
-	<div class="px-1" style="width: 30em">
-		<b-form-row>
-			<b-col>
-				<b-form-group
-					id="data-name"
-					class="mb-1"
-					label="姓名"
-				><b-form-input
-					v-model.trim="data.name"
-				/></b-form-group>
-			</b-col>
-			<b-col cols="8">
-				<b-form-group
-					id="data-position"
-					class="mb-1"
-					label="职位"
-				><b-form-input
-					v-model.trim="data.position"
-				/></b-form-group>
-			</b-col>
+	<b-row class="justify-content-center">
+		<b-col
+			lg="auto"
+			cols="12"
+			class="d-flex justify-content-center"
+		>
+			<component
+				:is="solutionList[0].NameCard"
+				:name="data.name"
+				:position="data.position"
+				:telephone="data.telephone"
+				:email="data.emailName"
+				:homeURL="data.homeURL"
+				:address="data.address"
+				ref="solution"
+			/>
+		</b-col>
 
-			<div class="w-100"></div>
+		<div class="w-100"></div>
 
-			<b-col>
-				<b-form-group
-					id="data-telephone"
-					class="mb-1"
-					label="联系方式"
-				><b-form-input
-					v-model.trim="data.telephone"
-				/></b-form-group>
-			</b-col>
+		<b-col lg="6" cols="9">
+			<b-form-row>
+				<b-col>
+					<b-form-group
+						id="data-name"
+						class="mb-1"
+						label="姓名"
+					><b-form-input
+						v-model.trim="data.name"
+					/></b-form-group>
+				</b-col>
+				<b-col cols="8">
+					<b-form-group
+						id="data-position"
+						class="mb-1"
+						label="职位"
+					><b-form-input
+						v-model.trim="data.position"
+					/></b-form-group>
+				</b-col>
 
-			<b-col>
-				<b-form-group
-					id="data-email"
-					class="mb-1"
-					label="邮箱账户名"
-				><b-form-input
-					v-model.trim="data.emailName"
-				/></b-form-group>
-			</b-col>
-		</b-form-row>
+				<div class="w-100"></div>
 
-		<b-button
-			variant="success"
-			block
-			class="mt-2"
-			@click="download"
-		>生成</b-button>
-	</div>
+				<b-col cols="3">
+					<b-form-group
+						id="data-telephone"
+						class="mb-1"
+						label="联系方式"
+					><b-form-input
+						v-model.trim="data.telephone"
+					/></b-form-group>
+				</b-col>
+
+				<b-col cols="3">
+					<b-form-group
+						id="data-email"
+						class="mb-1"
+						label="邮箱账户名"
+					><b-form-input
+						v-model.trim="data.emailName"
+					/></b-form-group>
+				</b-col>
+
+				<b-col>
+					<b-form-group
+						id="data-address"
+						class="mb-1"
+						label="地点"
+					><b-form-select
+						v-model.trim="data.address"
+						:options="addressList"
+					/></b-form-group>
+				</b-col>
+			</b-form-row>
+
+			<b-button
+				variant="success"
+				block
+				class="mt-2"
+				@click="download"
+			>生成</b-button>
+		</b-col>
+	</b-row>
 </b-container>
 </template>
 
